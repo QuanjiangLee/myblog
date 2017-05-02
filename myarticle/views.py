e1cc5218133a9ec2ab8c57371eb519c5e73188d7
Quanjiang Lee <QuanjiangLee@gmail.com>
2017-05-02 23:03:33 +0800

    添加登录页面，页面ui和功能优化

--- myarticle/views.py
+++ myarticle/views.py
@@ -7,7 +7,12 @@ from django.http import Http404
 # Create your views here.
 def home(request):
 	postList = myArticle.objects.all()
-	return render(request, 'home.html',{'postList':postList})
+	for post in postList:
+		if len(str(post.content)) > 200:
+			post.content = str(post.content)[0:200]
+		else:
+			continue
+	return render(request, 'home.html',{'postList':postList,'post.content':post.content})
 #    return HttpResponse("hello world,Django!")
 
 def  detail(request, id):
@@ -28,9 +33,13 @@ def detail(request, my_args):
 def templateTest(request):
 	return render(request,'templateTest.html',{'nowTime':datetime.now()})
 
+def login(request):
+	return render(request,'login.html')
+'''
 def archives(request):
 	try:
 		postList = myArticle.objects.all()
 	except myArticle.DoesNotExist:
 		raise Http404
 	return render(request,'archives.html',{'post_list' : postList,'error': False})
+'''
